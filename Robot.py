d927696a3f5cf4aafa55439be63a2674eba5a774
OistinR <oistinrutledge1@gmail.com>
2025-04-01 17:05:53 +0200

    robot work

--- Robot.py
+++ Robot.py
@@ -7,12 +7,10 @@ class Robot:
     l = 0.16 # distance between wheels in metres
     
     position = (0,0) # X, Y
-    angle = 0 
 
     def __init__(self, position, angle):
         self.position = position
         self.angle = angle
-        
 
     def forward_kinematics(self, x, y, angle, Vl, Vr):
         
@@ -27,24 +25,39 @@ class Robot:
                              [math.sin(w*delta_t),  math.cos(w*delta_t), 0],
                              [0                  ,  0                  , 1]])
         
+        print(icc)
         second_part = np.array([[x-icc[0]],[y-icc[1]],[angle]])
 
+        print(rotation.shape)
+        print(second_part.shape)
+
         third_part = np.array([[icc[0]],[icc[1]],[w*delta_t]])
+        print(np.dot(rotation,second_part) + third_part)
 
         return np.dot(rotation,second_part) + third_part
     
     def update(self):
-        # update location based on logic
-
-        pose = self.forward_kinematics(self.position[0],self.position,1,0,1)    
         
-        # return pose 
+        # logic stuff 
+        pose = self.forward_kinematics(self.position[0],self.position[1],self.angle,0,1)
+
+        self.angle = float(pose[2])
+        self.position = (float(pose[0]), float(pose[1]))
+    
+        
         return pose
 
 
 def main():
-    r = Robot()
-    r.update()
-    
+    r = Robot((0,0), 1)
+    # r.forward_kinematics(0,0,1,0,1)
+    for i in range(0,10):
+        print(r.update())
+
 if __name__ == '__main__':
     main()
+
+
+
+
+
