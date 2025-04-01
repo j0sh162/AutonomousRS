b0382ed82e7c63880abb7d732a1eb55a88aed4d4
OistinR <oistinrutledge1@gmail.com>
2025-04-01 17:16:48 +0200

    fixes division by zero

--- Robot.py
+++ Robot.py
@@ -14,7 +14,11 @@ class Robot:
 
     def forward_kinematics(self, x, y, angle, Vl, Vr):
         
-        R = (0.5*self.l)*((Vl+Vr)/(Vr-Vl)) 
+        if Vr - Vl == 0:
+            R = 1
+        else:
+            R = (0.5*self.l)*((Vl+Vr)/(Vr-Vl)) 
+
         w = (Vr-Vl)/(self.l)
 
         icc = [x - R*math.sin(angle), y +R*math.cos(angle)]
@@ -25,22 +29,24 @@ class Robot:
                              [math.sin(w*delta_t),  math.cos(w*delta_t), 0],
                              [0                  ,  0                  , 1]])
         
-        print(icc)
+        # print(icc)
         second_part = np.array([[x-icc[0]],[y-icc[1]],[angle]])
 
-        print(rotation.shape)
-        print(second_part.shape)
+        # print(rotation.shape)
+        # print(second_part.shape)
 
         third_part = np.array([[icc[0]],[icc[1]],[w*delta_t]])
-        print(np.dot(rotation,second_part) + third_part)
+        # print(np.dot(rotation,second_part) + third_part)
+
+       
 
         return np.dot(rotation,second_part) + third_part
     
     def update(self):
         
         # logic stuff 
-        pose = self.forward_kinematics(self.position[0],self.position[1],self.angle,0,1)
-
+        pose = self.forward_kinematics(self.position[0],self.position[1],self.angle,10,1)
+        print(pose)
         self.angle = float(pose[2])
         self.position = (float(pose[0]), float(pose[1]))
     
@@ -51,7 +57,7 @@ class Robot:
 def main():
     r = Robot((0,0), 1)
     # r.forward_kinematics(0,0,1,0,1)
-    for i in range(0,10):
+    for i in range(0,50):
         print(r.update())
 
 if __name__ == '__main__':
